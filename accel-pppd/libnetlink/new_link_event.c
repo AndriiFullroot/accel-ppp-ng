eed09bc3f0becbb3a41e0e691b1b52b3427a3e2b
RC <ritika0313@gmail.com>
2026-08-06 16:14:55 -0700

    Apply suggestions to tighten emit callback condition from code review
    Tightened the condition that emits add callback for net-link events in order to avoid triggering PPPoE service start attempts for transient/non-ready states like DORMANT or LOWERLAYERDOWN.
    
    Co-authored-by: RC <ritika0313@gmail.com>

--- accel-pppd/libnetlink/new_link_event.c
+++ accel-pppd/libnetlink/new_link_event.c
@@ -54,9 +54,17 @@ static int nnle_nlmsg_handler(const struct sockaddr_nl *nladdr,
 	  is not DOWN(it could be *UNKNOWN*) and interface info change is UP.
 	  That's how we can handle different interfaces provided by VPP - e.g., LCP tap and LCP bond.
 	 */
-	if (hdr->nlmsg_type == RTM_NEWLINK && (*(uint32_t *)RTA_DATA(tb[IFLA_OPERSTATE]) != IF_OPER_DOWN) &&
-	    (ifi->ifi_flags & ifi->ifi_change) & IFF_UP) {
-		nnle_emit_callbacks(ifname, 1);
+     if (hdr->nlmsg_type == RTM_NEWLINK
+            && tb[IFLA_OPERSTATE]
+            && RTA_PAYLOAD(tb[IFLA_OPERSTATE]) >= 1) {
+                unsigned char operstate = *(unsigned char *)RTA_DATA(tb[IFLA_OPERSTATE]);
+
+                if (operstate == IF_OPER_UP
+                    || (operstate == IF_OPER_UNKNOWN
+                        && (ifi->ifi_change & IFF_UP)
+                        && (ifi->ifi_flags & IFF_UP))) {
+                        nnle_emit_callbacks(ifname, 1);
+                }
 	} else if (hdr->nlmsg_type == RTM_DELLINK) {
 		nnle_emit_callbacks(ifname, 0);
 	}
